16e7b3e80a74fa497e0909be279087acf8867e71
lkwa201 <lkwa201@gmail.com>
2020-09-01 10:04:10 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    UserInfoBuilder builder = new UserInfoBuilder();
+        UserInfo user = builder.setName("addLee")
+                .setAge(35)
+                .setAddr("InCheon").build();
+        System.out.println(user.getUserInfo());
+    }
+}
